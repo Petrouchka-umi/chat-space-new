8c2b7ae889240ff8d72813c949912e57ad575304
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2020-07-23 21:34:32 +0900

    message-edit

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,19 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users_groups
   has_many :users, through: :users_groups
+
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
