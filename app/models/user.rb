c951ede33cda9054e0308818e3576e27175cd146
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2020-08-09 21:52:21 +0900

    search-commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ApplicationRecord
   has_many :users_groups
   has_many :groups, through: :users_groups
   validates :name, presence: true, uniqueness: true
+
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
+  
 end
 
